2ea590ee914fa4c6b23ce0825ae5231568616860
yuhlearn <yuhlearn@users.noreply.github.com>
2025-01-28 23:35:30 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="yuhlearn"
+original_name="videsrgan"
+original_urlname="videsrgan"
+original_description="Awesome videsrgan created by yuhlearn"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv videsrgan $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
